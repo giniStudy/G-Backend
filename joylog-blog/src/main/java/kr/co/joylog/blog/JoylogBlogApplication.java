b909f3edebb58f96f5f6d97a9e300c14a671a837
장태훈 <changtehoon@jangtaehun-ui-MacBookPro.local>
2022-07-07 01:21:23 +0900

    create backend project

---
+++ joylog-blog/src/main/java/kr/co/joylog/blog/JoylogBlogApplication.java
@@ -0,0 +1,13 @@
+package kr.co.joylog.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoylogBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoylogBlogApplication.class, args);
+	}
+
+}
---
+++ joylog-blog/src/test/java/kr/co/joylog/blog/JoylogBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.joylog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoylogBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
