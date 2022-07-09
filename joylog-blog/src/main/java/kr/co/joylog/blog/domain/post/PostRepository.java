88f119a266fe0dc3b7ddf7ebd66a183b995136a7
pointehd <pointehd@naver.com>
2022-07-09 16:50:18 +0900

    jpa import 삭제

--- joylog-blog/src/main/java/kr/co/joylog/blog/domain/post/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.co.joylog.blog.domain.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<PostEntity, Integer> {
-}
