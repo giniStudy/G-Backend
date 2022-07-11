3a99c616ffdb4b4a4648da19ec3a811e74237f58
pointehd <pointehd@naver.com>
2022-07-12 02:13:15 +0900

    테스트 주석처리

--- joylog-blog/src/test/java/kr/co/joylog/blog/JoylogBlogApplicationTests.java
+++ joylog-blog/src/test/java/kr/co/joylog/blog/JoylogBlogApplicationTests.java
@@ -3,7 +3,7 @@ package kr.co.joylog.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JoylogBlogApplicationTests {
 
 	@Test
